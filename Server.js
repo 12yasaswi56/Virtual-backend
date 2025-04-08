f7b122c8423f49974af7adaa4fc7e8e7f8ddf865
12yasaswi56 <yasaswikopparapu624@gmail.com>
2025-04-08 15:36:58 +0530

    fC

--- Server.js
+++ Server.js
@@ -92,8 +92,7 @@ app.get("/", (req, res) => {
 
 // 📌 Connect to MongoDB
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true ,ssl: true,
-    sslValidate: true })
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ MongoDB Atlas Connected Successfully"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
